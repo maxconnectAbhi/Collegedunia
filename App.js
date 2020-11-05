efd0e10a62070a09f2bff54452a4dc83d7b0402b
INTEC INFONET <abhi.maxconnect@gmail.com>
2020-11-06 01:56:44 +0530

    Task completed

--- App.js
+++ App.js
@@ -6,14 +6,21 @@ import {
   ScrollView,
   StatusBar,
   PermissionsAndroid,
+  FlatList,
+  TouchableOpacity,
 } from 'react-native';
 import {connect} from 'react-redux';
 
 import NetInfo from "@react-native-community/netinfo";
 import Geolocation from '@react-native-community/geolocation';
 import { getCurrentweather,getweatherForcast } from './App/redux/actionCreators/weatherCreators';
+import AsyncStorage from '@react-native-community/async-storage';
 
-
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
 
  class App extends Component {
 
@@ -24,6 +31,11 @@ import { getCurrentweather,getweatherForcast } from './App/redux/actionCreators/
         {title:'Need Permission',
       messag:'Please allow location permission to see weather near you.'}
       )
+      if ('granted' === PermissionsAndroid.RESULTS.GRANTED) {
+        this.getCurrentWeather()
+      }else{
+        alert("Permission Denied");
+      }
     } catch (error) {
       console.log(error)
     }
@@ -32,49 +44,77 @@ import { getCurrentweather,getweatherForcast } from './App/redux/actionCreators/
       if(state.isConnected) {
        this.getCurrentWeather()
       }else{
-     
+     this.emptyComponent()
       }
     })
+  }
+
+  async componentWillUnmount(){
+    await AsyncStorage.clear()
   }
 
   async getCurrentWeather(){
     Geolocation.getCurrentPosition(
       (position) => {
+      console.log('position= ',position);
+
        this.props.getCurrentweather(position.coords.latitude,position.coords.longitude)
        this.props.getweatherForcast(position.coords.latitude,position.coords.longitude)
         },
-      (error) => this.setState({ error: error.message }),
+      (error) => console.log('error= ',error),
       { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 }
     );
   }
+
+  emptyComponent(){
+    return(
+      <View style={styles.container}>
+      <Text style={styles.currentText}>Something Went Wrong at our End</Text>
+<TouchableOpacity style={styles.button} onPress={()=>this.getCurrentWeather()}>
+<Text>RETRY</Text>
+</TouchableOpacity>
+      </View>
+    )
+  }
+
+  getDayOfWeek(i){
+    let date = new Date();
+    date = date.addDays(i);
+    const dayOfWeek = date.getDay();    
+    return isNaN(dayOfWeek) ? null : 
+      ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+}
 
   render() {
     if(this.props.loading){
       return(
       <View style={styles.container}>
-      <Image style={{height:'40%',width:'40%'}} resizeMode='contain' source={require('./App/assets/loader.gif')}/>
+      <Image style={{height:200,width:200}} resizeMode='contain' source={require('./App/assets/loader.gif')}/>
       </View>
       )
     }
     else{
     return (
-      <View style={styles.container}>
+      <View style={[styles.container,{justifyContent:'space-between'}]}>
         <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic">
+       
          <View style={styles.container}>
           <Text style={styles.currentText}>{this.props.currentWeatherData?.main?.temp}</Text>
           <Text style={styles.boldText}>{this.props.currentWeatherData?.name}</Text>
           </View>
-          <View style={{flex:1}}>
-          <View style={styles.row}>
-          <Text style={styles.boldText}>{this.props.weatherData?.name}</Text>
-          <Text style={styles.boldText}>{this.props.weatherData?.name}</Text>
+       <FlatList
+       style={{flex:1}}
+       data={this.props.weatherData.filter((value, index, arr) => index > 0 && index < 6)}
+       ListEmptyComponent={this.emptyComponent()}
+       keyExtractor={(item) => String(item.dt)}
+       renderItem={({item,index}) =>
+       <View style={styles.row}>
+          <Text style={[styles.boldText,{width:'70%'}]}>{this.getDayOfWeek(index+1)}</Text>
+          <Text style={[styles.boldText,{width:'20%'}]}>{item.temp.day}</Text>
           </View>
-
-          </View>
-        </ScrollView>
+       }
+       />
       </SafeAreaView>
       </View>
     )
@@ -85,7 +125,7 @@ import { getCurrentweather,getweatherForcast } from './App/redux/actionCreators/
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,backgroundColor:'#FFF',
+    flex:1.4,backgroundColor:'#FFF',
     justifyContent:'center',alignItems:'center'
   },
   currentText:{
@@ -94,13 +134,19 @@ const styles = StyleSheet.create({
   },
   boldText:{
     fontWeight:'bold',
-    fontSize:22
+    fontSize:22,
   },
   row:{
     flexDirection:'row',
     justifyContent:'space-around',
-    borderBottomWidth:1,
-    flex:1/5
+    borderTopWidth:1,
+    padding:12,
+    width:'100%'
+  },
+  button:{
+    borderWidth:1,
+    padding:10,
+    marginTop:'20%'
   }
  
 });
